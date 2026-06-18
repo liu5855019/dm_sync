7e9942469aa53f615e1b20162f96e8085302439b
DaiMu <258141764@qq.com>
2026-06-18 15:26:44 +0800

    edit level

---
+++ CaiKuang/tscns/level/level3_data.gd
@@ -0,0 +1,28 @@
+﻿static func get_data() -> Array:
+    return [
+        PlayerData.new(1, Vector2i(2, 12)),
+        PlayerData.new(1, Vector2i(5, 12)),
+        OreData.new(2, Vector2i(3, 0)),
+        OreData.new(3, Vector2i(3, 1)),
+        OreData.new(3, Vector2i(2, 8)),
+        OreData.new(4, Vector2i(2, 6)),
+        OreData.new(2, Vector2i(2, 7)),
+        OreData.new(4, Vector2i(3, 2)),
+        OreData.new(4, Vector2i(3, 6)),
+        OreData.new(1, Vector2i(3, 8)),
+        OreData.new(2, Vector2i(3, 5)),
+        StoneData.new(Vector2i(3, 3)),
+        StoneData.new(Vector2i(3, 4)),
+        OreData.new(5, Vector2i(2, 5)),
+        OreData.new(4, Vector2i(0, 5)),
+        OreData.new(4, Vector2i(0, 6)),
+        OreData.new(4, Vector2i(0, 4)),
+        OreData.new(2, Vector2i(0, 1)),
+        OreData.new(2, Vector2i(0, 2)),
+        StoneData.new(Vector2i(0, 8)),
+        OreData.new(1, Vector2i(2, 3)),
+        OreData.new(4, Vector2i(2, 4)),
+        StoneData.new(Vector2i(2, 2)),
+        OreData.new(1, Vector2i(0, 9)),
+        StoneData.new(Vector2i(2, 0))
+    ]
